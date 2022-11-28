1ed9fc678d4b59cfea3d95ed10c227eb4e8efaa7
myl <myldast1@126.com>
2022-11-28 23:49:05 +0800

    reset quiz btn added

--- flutter_complete_guide_fighting/lib/main.dart
+++ flutter_complete_guide_fighting/lib/main.dart
@@ -50,6 +50,13 @@ class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
   var _totalScore = 0;
 
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -74,7 +81,7 @@ class _MyAppState extends State<MyApp> {
                       questionIndex: _questionIndex,
                       answerQuestion: _answerQuestion,
                     )
-                  : Result(_totalScore),
+                  : Result(_totalScore, _resetQuiz),
         ),
       ),
     );
--- flutter_complete_guide_fighting/lib/result.dart
+++ flutter_complete_guide_fighting/lib/result.dart
@@ -2,13 +2,41 @@ import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
   // const Rlt({ Key? key }) : super(key: key);
-  final score;
-  Result(this.score);
+  final int score;
+  final VoidCallback resetQuizHandler;
+
+  Result(this.score, this.resetQuizHandler);
+
+  String get content {
+    String text;
+    if (score < 16) {
+      text = "you did it! small score:$score";
+    } else {
+      text = "you did it! big score:$score";
+    }
+    return text;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text("you did it! Total score:$score"),
+      child: Column(
+        children: [
+          Text(
+            content,
+            style: TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(
+            child: Text("reset quiz"),
+            onPressed: resetQuizHandler,
+            textColor: Colors.blue,
+          )
+        ],
+      ),
     );
   }
 }

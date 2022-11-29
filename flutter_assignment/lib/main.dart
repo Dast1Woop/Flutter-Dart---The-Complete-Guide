787d1b9727183b9a54a6be3fa7cd1e2e928724c3
myl <myldast1@126.com>
2022-11-30 00:10:15 +0800

    assignment do part

---
+++ flutter_assignment/lib/main.dart
@@ -0,0 +1,53 @@
+// 1) Create a new Flutter App (in this project) and output an AppBar and some text
+// below it
+// 2) Add a button which changes the text (to any other text of your choice)
+// 3) Split the app into three widgets: App, TextControl & Text
+
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  // const Myapp({ Key? key }) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var _content = "content";
+
+  void _btnDidClick() {
+    setState(() {
+      _content = "content changed1";
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Title",
+          ),
+        ),
+        body: Column(
+          children: [
+            Text(
+              _content,
+            ),
+            RaisedButton(
+              onPressed: _btnDidClick,
+              child: Text(
+                "btn title",
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}

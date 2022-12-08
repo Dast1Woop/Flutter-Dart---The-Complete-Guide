3a595c6ea95fc3cf668d18402772e27df57cdedf
myl <myldast1@126.com>
2022-12-09 01:15:55 +0800

    new class and ... syntax

--- flutter_guide_section4/lib/main.dart
+++ flutter_guide_section4/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_guide_section4/transaction.dart';
 
 void main(List<String> args) {
   runApp(App());
@@ -7,6 +8,33 @@ void main(List<String> args) {
 
 class App extends StatelessWidget {
   // const App({ Key? key }) : super(key: key);
+
+  final List<Transaction> transactionsArr = [
+    Transaction(
+      id: "id0",
+      title: "title0",
+      price: 1,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "id1",
+      title: "title0",
+      price: 2,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "id2",
+      title: "title0",
+      price: 11,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "id3",
+      title: "title0",
+      price: 12,
+      date: DateTime.now(),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -18,23 +46,22 @@ class App extends StatelessWidget {
           ),
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.end,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Container(
-              child: Card(
-                color: Colors.blue,
-                child: Text("content1"),
-                elevation: 5, //阴影
-              ),
-              width: double.infinity,
-              alignment: Alignment.center,
-              color: Colors.orange,
-            ),
-            Card(
-              color: Colors.yellow,
-              child: Text("content2"),
-            ),
+            //数组解包+匹配语法格式：...(arr).map((e){xx})
+            ...(transactionsArr).map((e) {
+              return Container(
+                child: Card(
+                  color: Colors.orange,
+                  child: Text(e.id + " " + e.title + " " + '${e.price}'),
+                  elevation: 5, //阴影
+                ),
+                width: double.infinity,
+                alignment: Alignment.center,
+                color: Colors.orange,
+              );
+            }).toList(),
           ],
         ),
       ),
